68f61babe8535ca846a32cd7e3a95e472c0d14cb
Meshiest <gpg@reheatedcake.io>
2025-06-12 19:26:50 -0400

    test: add decoder7seg module test

--- src/compiler_tests.rs
+++ src/compiler_tests.rs
@@ -27,3 +27,24 @@ module example(a, b, c, d) -> e {
     let out = c.compile("example").expect("Failed to compile module");
     println!("Source:\n{source}\n\n{out}");
 }
+
+#[test]
+fn compile_7seg() {
+    let p = bearilog::ModuleParser::new();
+    let source = "
+module decoder7seg(a0, a1, a2, a3) -> a, b, c, d, e, f, g {
+    a = a3 || (a2 && a0) || (a1 && !a0) || (!a2 && !a0);
+    b = (!a2 && !a0) || (!a1 && a0) || (a1 && !a0) || (a2 && !a1);
+    c = (!a2 && a0) || (!a1 && a0) || (a2 && !a1) || a3 || (a1 && !a0);
+    d = (!a1 && !a0) || (a2 && !a1 && a0) || (!a2 && a1 && !a0) || (!a2 && a1 && a0) || (a3 && !a0);
+    e = !a0 || (a2 && !a1) || (a3 && a2);
+    f = (a1 && !a0) || (!a2 && !a0) || (a3 && a2) || (a3 && a1) || (a2 && !a1);
+    g = (!a2 && a1) || (a1 && !a0) || (a3 && a2) || (a2 && a0) || (a3 && a1);
+}
+    "
+    .trim();
+    let module = p.parse(source).expect("Failed to parse module");
+    let mut c = Compiler::new([module]);
+    let out = c.compile("decoder7seg").expect("Failed to compile module");
+    println!("Source:\n{source}\n\n{out}");
+}
