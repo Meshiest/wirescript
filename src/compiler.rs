bd56996c442c955c34b8d61560998f9c8447d1f5
Isaac <gpg@reheatedcake.io>
2025-06-12 01:44:35 -0400

    feat(compile): input/output validation, add todos for statement handling

--- src/compiler.rs
+++ src/compiler.rs
@@ -3,7 +3,7 @@ use std::{collections::HashMap, sync::Arc};
 use thiserror::Error;
 
 use crate::{
-    ast::AstModule,
+    ast::{AstModule, Literal},
     wires::{Gate, Wire, WireConnection},
 };
 
@@ -30,6 +30,34 @@ pub struct CompiledModule {
 pub enum CompileError {
     #[error("unknown module {0}")]
     UnknownModule(String),
+    #[error("variable {0} is already in use: {1:?}")]
+    VariableInUse(String, InUseReason),
+}
+
+#[derive(Debug)]
+pub enum InUseReason {
+    DuplicateInput,
+    DuplicateOutput,
+    DuplicateBuffer,
+    DuplicateConst,
+    InputWithSimilarName,
+    ConstWithSimilarName,
+    OutputWithSimilarName,
+}
+
+enum PendingConnection {
+    /// A connection from input (as a source)
+    Input(String),
+    /// An immediate value to be inserted into the gate
+    Const(Literal),
+    /// A gate and its property
+    Gate(Arc<Gate>, String),
+}
+
+/// A type of assignable variable
+enum Slot {
+    Output,
+    Buffer,
 }
 
 impl Compiler {
@@ -46,11 +74,55 @@ impl Compiler {
     }
 
     pub fn compile(&self, target: &str) -> Result<CompiledModule, CompileError> {
-        if !self.ast_modules.contains_key(target) {
+        let Some(ast) = self.ast_modules.get(target) else {
             return Err(CompileError::UnknownModule(target.to_owned()));
+        };
+
+        // The scope will be used to lookup existing wire output connections
+        // Adding new consts or buffers will introduce new variables into the scope
+        let mut scope: HashMap<String, PendingConnection> = Default::default();
+        for i in &ast.inputs {
+            if scope.contains_key(i) {
+                return Err(CompileError::VariableInUse(
+                    i.to_owned(),
+                    InUseReason::DuplicateInput,
+                ));
+            }
+            scope.insert(i.to_owned(), PendingConnection::Input(i.to_owned()));
         }
 
+        // A map of available slots (outputs or buffers) that can receive
+        // a value ONCE.
+        let mut slots: HashMap<String, (Slot, Option<PendingConnection>)> = Default::default();
+        for o in &ast.outputs {
+            if scope.contains_key(o) {
+                return Err(CompileError::VariableInUse(
+                    o.to_owned(),
+                    InUseReason::InputWithSimilarName,
+                ));
+            }
+            if slots.contains_key(o) {
+                return Err(CompileError::VariableInUse(
+                    o.to_owned(),
+                    InUseReason::DuplicateOutput,
+                ));
+            }
+
+            // This slot has not been assigned yet
+            slots.insert(o.to_owned(), (Slot::Output, None));
+        }
+
+        let mut gates: Vec<Arc<Gate>> = Vec::new();
+        let mut wires: Vec<WireConnection> = Vec::new();
+
+        // TODO: statements:
+        // TODO: walk consts and add new slots
+        // TODO: when a module is referenced, compile/clone it and append gates/wires, then hook in inputs/outputs
+        // TODO: walk buffers and add slots and scope (as they are inputs and outputs)
+
         // TODO: assemble rerouters for module inputs
+
+        // TODO: warnings if not all slots have assignments
 
         todo!()
     }
