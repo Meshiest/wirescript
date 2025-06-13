e5348be7697e91314caeb2acfbc0d72f11de1a69
Isaac <gpg@reheatedcake.io>
2025-06-13 00:24:15 -0400

    feat: args parsing and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+## Bearilog
+
+1. Install rust
+2. Build a module `cargo run ./examples/cpu reg64`
+3. Generate a digraph`cargo run ./examples/cpu reg64 -d`
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::error::Error;
 
+use clap::Parser;
+use clap_stdin::FileOrStdin;
 use lalrpop_util::lalrpop_mod;
 
 use crate::wires::CompiledModule;
@@ -16,13 +18,47 @@ mod helpers;
 
 lalrpop_mod!(pub bearilog);
 
-fn main() {
-    println!("Hello, world!");
+/// Program to parse bearilog to gates
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to the module (or stdin)
+    file: FileOrStdin,
+
+    /// The module to parse
+    module: String,
+
+    #[arg(short, long)]
+    digraph: bool,
 }
 
-pub fn compile_module(source: &'static str, name: &str) -> Result<CompiledModule, Box<dyn Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+    let source = args.file.contents()?;
+    let modules = bearilog::ModulesParser::new()
+        .parse(&source)
+        .map_err(|e| e.to_string())?;
+    let mut compiler = compiler::Compiler::new(modules);
+    let res = match compiler.compile(&args.module) {
+        Ok(res) => res,
+        Err(e) => {
+            eprintln!("{e}");
+            Err(e)?
+        }
+    };
+
+    if args.digraph {
+        println!("{}", res.digraph()?);
+    } else {
+        println!("{res}");
+    }
+
+    Ok(())
+}
+
+pub fn compile_module(source: &str, name: &str) -> Result<CompiledModule, Box<dyn Error>> {
     let p = bearilog::ModuleParser::new();
-    let parsed_module = p.parse(source)?;
+    let parsed_module = p.parse(source).map_err(|e| e.to_string())?;
     let mut compiler = compiler::Compiler::new([parsed_module]);
     Ok(compiler.compile(name)?)
 }
